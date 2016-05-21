3693c55badeb8c7abccc4b83e2f96cea74305ebf
URobert <axius_robert@yahoo.com>
2016-05-21 13:35:50 +0300

    Primitives and arrays updated

--- 01.primitives.js
+++ 01.primitives.js
@@ -14,28 +14,55 @@ var nil   = null;
 var undef = undefined;
 // Symbol (ES6)
 var symb  = Symbol('some description');
+var undefv = "";
+
+//console.log(typeof nr);
+//console.log(typeof str);
+//console.log(typeof bool);
+//console.log(typeof nil);
+//console.log(typeof undef);
+//console.log(typeof symb);
+
+//variables
+// -they are case sensitives
 
 
-/**
- *  variables
- */
-
-
-/**
- *  typeof operator
- */
-
-
+//typeof operator
+// -operator, AND NOT a function
+ 
 /**
  *  Loosely typed, Dynamic language
  */
-
+//var str = 1;
+//implicit type coercion ex: '1' = 1;
 
 /**
  *  type casting
  */
-
+//var age= "34";
+//age = parseInt(age);
+//var username = 1122;
+//username = String(username);
+//console.log(0 + username);
 
 /**
  *  falsey values
  */
+//var exists = 1;
+//console.log(!exists);
+
+//if (""){
+//    //code
+//    console.log("ok");
+//}else{
+//    console.log("nay");
+//}
+
+
+console.log( !! "" );
+console.log( !!0) ;
+console.log( !!false );
+console.log( !!undefined );
+console.log( !!null );
+console.log( !!NaN);
+
--- 02.arrays.js
+++ 02.arrays.js
@@ -4,9 +4,13 @@
 
 
 // array literal
-var arr = [1, 2];
-
-
+var arr = [1, 2,3];
+//var arr2 = new Array(4);
+//
+//console.log(arr);
+//console.log(arr2);
+//console.log(arr[0]);
+//console.log(arr2[0]);
 /**
  *  element access
  */
@@ -15,16 +19,33 @@ var arr = [1, 2];
 /**
  *  length
  */
+//arr[arr.length] = 3;
+//arr.a = 5;
+//arr['b']= 5; bad practice
 
+/*
+console.log(arr.length);
+console.log(arr)*/;
+
+//var emptyArr = [];
+//emptyArr[4] = 4;
+//console.log(emptyArr);
+//console.log(emptyArr.length);
 
 /**
  *  stack
  */
-
+//console.log( arr.push(3) );
+//console.log( arr.push(4) );
+//console.log( arr.push(5) );
+//console.log(arr);
 
 /**
  *  queue
  */
+//console.log(arr.shift());
+//console.log(arr.shift())
+//console.log(arr);
 
 
 /**
@@ -35,21 +56,43 @@ var arr = [1, 2];
 /**
  *  sub-arrays
  */
+//arr = arr.slice(0,2);
 
+//console.log(arr.splice(1,0,'a','b'));
+//console.log(arr);
 
 /**
  *  find index
  */
-
+console.log(arr);
+//console.log(arr.indexOf(4) > -1);
 
 /**
  *  filter
  */
-
+//var arr2 = arr.filter(function (element,index,array){
+//    return element > 1; //console.log(element,index,array);    
+//});
+//console.log("filtered array", arr2);
+//!!! Filter nu modifica continutul unul array
 
 /**
  *  iterations
  */
+//for (var i = 0; i <= 5; i+=1) {
+//    console.log(arr[i]);
+//}
+//var newArray = [];
+//arr.forEach(function(element,index,array){
+//  newArray.push(element+10);
+//});
+//console.log(newArray);
+
+var mappedArr = arr.map(function(element,index,array){
+    return element + 10; 
+});
+console.log(mappedArr);
+
 
 
 /**
@@ -60,3 +103,5 @@ var arr = [1, 2];
 /**
  *  typeof
  */
+console.log(typeof arr);
+console.log( Array.isArray(arr));
